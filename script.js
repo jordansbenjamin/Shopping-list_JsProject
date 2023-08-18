e5862c7698aa72e06c2fa225939e0ba8e9061dda
jordansbenjamin <14327@coderacademy.edu.au>
2023-08-18 21:34:40 +1000

    Implemented and tested adding items to list

---
+++ script.js
@@ -0,0 +1,47 @@
+// Element selections
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+
+function addItem(e) {
+	e.preventDefault();
+
+	const newItem = itemInput.value;
+
+	// Validate Input
+	if (newItem === "") {
+		alert("Please add an item");
+		return;
+	}
+	// console.log('success');
+
+	// Create list item
+	const li = document.createElement("li");
+	li.appendChild(document.createTextNode(newItem));
+	// console.log(li);
+
+	const button = createButton("remove-item btn-link text-red");
+    // console.log(button);
+    li.appendChild(button);
+
+    itemList.appendChild(li);
+
+    itemInput.value = '';
+}
+
+function createButton(classes) {
+	const button = document.createElement("button");
+	button.className = classes;
+	const icon = createIcon("fa-solid fa-xmark");
+    button.appendChild(icon)
+	return button;
+}
+
+function createIcon(classes) {
+	const icon = document.createElement("i");
+	icon.className = classes;
+	return icon;
+}
+
+// Event listeners
+itemForm.addEventListener("submit", addItem);
