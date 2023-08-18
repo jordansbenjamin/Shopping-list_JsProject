6ebe852455a9ff6ff1f30d7d086cfe5225a592be
jordansbenjamin <14327@coderacademy.edu.au>
2023-08-18 21:43:15 +1000

    Added comments

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
 
 function addItem(e) {
+    // Prevents default behaviour of form submit event
 	e.preventDefault();
 
+    // New item value from the input field
 	const newItem = itemInput.value;
 
 	// Validate Input
@@ -15,29 +17,37 @@ function addItem(e) {
 	}
 	// console.log('success');
 
-	// Create list item
+	// Create new list item
 	const li = document.createElement("li");
 	li.appendChild(document.createTextNode(newItem));
 	// console.log(li);
 
+    // Create new button
 	const button = createButton("remove-item btn-link text-red");
     // console.log(button);
+    // Add new button to list
     li.appendChild(button);
 
+    // Add the new list item to the overall itemList
     itemList.appendChild(li);
 
+    // Reset input value
     itemInput.value = '';
 }
 
 function createButton(classes) {
+    // Create new btn element
 	const button = document.createElement("button");
 	button.className = classes;
+    // Add create icon element
 	const icon = createIcon("fa-solid fa-xmark");
+    // To append it inside the button
     button.appendChild(icon)
 	return button;
 }
 
 function createIcon(classes) {
+    // Create new icon element
 	const icon = document.createElement("i");
 	icon.className = classes;
 	return icon;
