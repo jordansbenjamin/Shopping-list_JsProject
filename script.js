c7759f1426afbfefe7f104f5287eab7e3bc4739a
jordansbenjamin <14327@coderacademy.edu.au>
2023-08-18 21:52:40 +1000

    Implemented logic for clearing & removing items

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
 
 function addItem(e) {
-    // Prevents default behaviour of form submit event
+	// Prevents default behaviour of form submit event
 	e.preventDefault();
 
-    // New item value from the input field
+	// New item value from the input field
 	const newItem = itemInput.value;
 
 	// Validate Input
@@ -22,36 +23,57 @@ function addItem(e) {
 	li.appendChild(document.createTextNode(newItem));
 	// console.log(li);
 
-    // Create new button
+	// Create new button
 	const button = createButton("remove-item btn-link text-red");
-    // console.log(button);
-    // Add new button to list
-    li.appendChild(button);
+	// console.log(button);
+	// Add new button to list
+	li.appendChild(button);
 
-    // Add the new list item to the overall itemList
-    itemList.appendChild(li);
+	// Add the new list item to the overall itemList
+	itemList.appendChild(li);
 
-    // Reset input value
-    itemInput.value = '';
+	// Reset input value
+	itemInput.value = "";
 }
 
 function createButton(classes) {
-    // Create new btn element
+	// Create new btn element
 	const button = document.createElement("button");
 	button.className = classes;
-    // Add create icon element
+	// Add create icon element
 	const icon = createIcon("fa-solid fa-xmark");
-    // To append it inside the button
-    button.appendChild(icon)
+	// To append it inside the button
+	button.appendChild(icon);
 	return button;
 }
 
 function createIcon(classes) {
-    // Create new icon element
+	// Create new icon element
 	const icon = document.createElement("i");
 	icon.className = classes;
 	return icon;
 }
 
+function removeItem(e) {
+	// console.log(e.target);
+	if (e.target.parentElement.classList.contains("remove-item")) {
+		e.target.parentElement.parentElement.remove();
+	}
+}
+
+function clearItems() {
+    // console.log('works');
+    // itemList.innerHTML = '';
+    // While the item list has a first child
+    while (itemList.firstChild) {
+        // remove the first child from the itemlist 
+        itemList.removeChild(itemList.firstChild)
+    }
+}
+
 // Event listeners
 itemForm.addEventListener("submit", addItem);
+
+itemList.addEventListener("click", removeItem);
+
+clearBtn.addEventListener("click", clearItems);
